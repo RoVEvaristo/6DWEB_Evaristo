c63e7bc7504c2b5abe353bdae6115f155c8f4384
Rainier Evaristo <dizonrr478@gmail.com>
2025-12-01 23:07:20 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require "includes/header.php";
+require "includes/footer.php";
+$username = "Ivy";
+$greetings = "Hello";
+$offer = array("Chocolate", 5, 25, 20);
+$usual_price = $offer[1] * $offer[2];
+$offer_price = $offer[1] * $offer[3];
+$saving = $usual_price - $offer_price;
+?>
+<!DOCTYPE html>
+    <html>
+        <body>
+            <h2>MULTI-BUY OFFER</h2>
+            <p><?= $greetings;?>, <?= $username;?></p>
+            <p>Buy <?= $offer[1]?> packs of <?= $offer[0]?> for $<?= $offer[3]?><br>
+            (usual price $<?= $offer[2] ?>)
+            </p>
+            <h1 class = button>Save $<?= $offer[1] ?></h1>
+        </body>
+    </html>
